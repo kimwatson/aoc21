cbde09978dc56fb863ca0cc39735431e7cb32b1a
Kim Watson <kimwatson@gmail.com>
2021-12-11 00:49:17 +0100

    Both parts are now solved

--- 02.cpp
+++ 02.cpp
@@ -4,33 +4,62 @@
 #include <fstream>
 #include <sstream> 
 
+void PartOneSingleLineAgrigator(const char direction, const int units, int& x, int& z)
+{
+    if(direction == 'f')
+    {
+        x += units;
+    }
+    else if(direction == 'd')
+    {
+        z += units;
+    }
+    else if(direction == 'u')
+    {
+        z -= units;
+    }
+}
+
+void PartTwoSingleLineAgrigator(const char direction, const int units, int& x, int& z, int& aim)
+{
+    if(direction == 'f')
+    {
+        x += units;
+        z += aim * units;
+    }
+    else if(direction == 'd')
+    {
+        aim += units;
+    }
+    else if(direction == 'u')
+    {
+        aim -= units;
+    }
+}
+
 int main()
 {
     std::ifstream file("02.txt");
     std::stringstream buffer;
     buffer << file.rdbuf();
 
-    int x = 0;
-    int z = 0;
+    int partOneX = 0;
+    int partOneZ = 0;
+ 
+    int partTwoX = 0;
+    int partTwoZ = 0;
+    int aim = 0;
 
     std::string directionPair;
     while (std::getline(buffer, directionPair, '\n')) 
     {
-        int distance = std::stoi(&directionPair.back());
+        const char direction = directionPair.front();
+        const int units = std::stoi(&directionPair.back());
 
-        if(directionPair.front() == 'f')
-        {
-            x += distance;
-        }
-        else if(directionPair.front() == 'd')
-        {
-            z += distance;
-        }
-        else if(directionPair.front() == 'u')
-        {
-            z -= distance;
-        }
+        PartOneSingleLineAgrigator(direction, units, partOneX, partOneZ);
+        PartTwoSingleLineAgrigator(direction, units, partTwoX, partTwoZ, aim);
     }
 
-    std::cout << x << " , " << z << " : " << x*z << "\n";
+    std::cout << "Part 1: " << partOneX << " , " << partOneZ << " : " << partOneX*partOneZ << "\n";
+    std::cout << "Part 2: " << partTwoX << " , " << partTwoZ << " : " << partTwoX*partTwoZ << "\n";
 }
